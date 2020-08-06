f212d08bd2cbbbe6dc766c4512484b81cbd2fade
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-08-06 12:37:31 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes />
+      <h2> Sua plataforma de estudos online </h2>
+    </div>
+  );
+}
